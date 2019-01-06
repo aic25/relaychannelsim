93abb4e4a4d857ec338522b23e403267f4a20537
canbolat <ihsan25@gmail.com>
2019-01-06 20:55:22 +0900

    initial commit

---
+++ WHadamard.cpp
@@ -0,0 +1,154 @@
+#include "const.h"
+extern errno_t err;
+
+void WHadamard_initialize(double(*transmitted_signal)[BS][BURST][2], double(*wh_sequence)[Np][N][2]){
+
+	if (pow(2, Np_POWER2) != Np || Np > 256){
+		printf("Make Np power of 2 and less than 300.\nAlso check if 2^Np_POWER2 = Np.\n");
+		getchar();
+		exit(1);
+	}
+	int code[Np][Np];
+	int codei[Np][Np];
+	int h_init, v_init, h, v, Pow;
+	for (size_t i = 0; i < Np; i++){
+		for (size_t j = 0; j < Np; j++){
+			code[i][j] = 0;
+		}
+	}
+	code[0][0] = 1;
+	for (int step = 0; step < Np_POWER2; step++){
+		Pow = intPow(2, step);
+		for (int matrix_quarter = 0; matrix_quarter < 4; matrix_quarter++){
+			h_init = matrix_quarter % 2;
+			v_init = matrix_quarter > 1 ? 1 : 0;
+			h_init = h_init*Pow;
+			v_init = v_init*Pow;
+			for (int h_index = 0; h_index < Pow; h_index++){
+				for (int v_index = 0; v_index < Pow; v_index++){
+					h = h_init + h_index;
+					v = v_init + v_index;
+					code[h][v] = (matrix_quarter == 3) ? -code[h_index][v_index] : code[h_index][v_index];
+				}
+			}
+		}
+	}	/* end :: Hadamard matrix generation */
+	for (h = 0; h < Np; h++){
+		for (v = 0; v < Np; v++){
+			codei[h][v] = code[h][Np - v - 1];
+		}
+	}
+	for (int f_index = 0; f_index < N; f_index++){	/* Only use first two sequences. same for all sub-carriers. */
+		for (int m = 0; m < Np; m++){
+			wh_sequence[0][m][f_index][0] = OneBySqrt2 * code[WH_CS1][m];	/* wh_sequence[transmitter index][symbol index][subcarrier index] */
+			wh_sequence[0][m][f_index][1] = OneBySqrt2 * codei[WH_CS1][m];
+			wh_sequence[1][m][f_index][0] = OneBySqrt2 * code[WH_CS2][m];
+			wh_sequence[1][m][f_index][1] = OneBySqrt2 * codei[WH_CS2][m];
+#if(BS==3)
+			wh_sequence[2][m][f_index] = code[WH_CS3][m];  
+#elif(BS>3)
+			printf("\n\nBS > 3 is not available.\n");
+			getchar();
+			exit(1);
+#endif
+		}
+	}	/* end :: chose codes to use */
+
+	int index;
+	double time_signal[BS][Nf][2];
+	double FFT_signal[BS][Nf][2];
+	double IFFT_signal[BS][Nf][2];
+
+	/* ~~~~~~~~~~~~~~Sending~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+	for (int m = 0; m < Np; m++){//1	
+		for (int n = 0; n < Nf; n++) {
+			for (size_t bs = 0; bs < BS; bs++)
+			{
+				FFT_signal[bs][n][0] = 0.0;
+				FFT_signal[bs][n][1] = 0.0;
+			}
+		}
+
+		for (int n = 0; n < N / 2; n++) {	/* ?Why */
+			for (size_t bs = 0; bs < BS; bs++)
+			{
+				FFT_signal[bs][n][0] = wh_sequence[bs][m][n][0];
+				FFT_signal[bs][n][1] = wh_sequence[bs][m][n][1];
+			}
+		}
+
+		for (int n = N / 2; n < N; n++) {	/* ?Why */
+			for (size_t bs = 0; bs < BS; bs++)
+			{
+				FFT_signal[bs][n + Nf - N][0] = wh_sequence[bs][m][n][0];
+				FFT_signal[bs][n + Nf - N][1] = wh_sequence[bs][m][n][1];
+			}
+		}
+
+		for (size_t bs = 0; bs < BS; bs++)
+		{
+			IFFT(FFT_signal[bs], IFFT_signal[bs]);	/* Swapped two parts in frequency domain and go back to time domain */
+		}
+
+		for (int n = 0; n < Nf; n++){
+			for (size_t bs = 0; bs < BS; bs++)
+			{
+				time_signal[bs][n][0] = IFFT_signal[bs][n][0];
+				time_signal[bs][n][1] = IFFT_signal[bs][n][1];
+			}
+		}
+
+		index = m * SAMPLEN;
+
+		for (int i = 0; i < GI; i++){
+			for (size_t bs = 0; bs < BS; bs++)
+			{
+				for (size_t ts = 0; ts < TS; ts++)
+				{
+					transmitted_signal[ts][bs][i + index][0] = time_signal[bs][Nf - GI + i][0];
+					transmitted_signal[ts][bs][i + index][1] = time_signal[bs][Nf - GI + i][1];
+				}
+			}
+		}
+		for (int i = 0; i < Nf; i++){
+			for (size_t bs = 0; bs < BS; bs++)
+			{
+				for (size_t ts = 0; ts < TS; ts++)
+				{
+					transmitted_signal[ts][bs][i + GI + index][0] = time_signal[bs][i][0];
+					transmitted_signal[ts][bs][i + GI + index][1] = time_signal[bs][i][1];
+				}
+			}
+		}
+	}
+}	/* end :: WH_initialize */
+
+void WH_channel_estimation(double(*DFT_signal)[TS][N][2], double(*H)[TS][BS][2], double(*wh_sequence)[Np][N][2]){
+	double temp[2], whsum;
+	temp[0] = temp[1] = 0; whsum = 0;
+	for (int tx = 0; tx < BS; tx++){
+		for (int rx = 0; rx < TS; rx++){
+			for (int f_index = 0; f_index < N; f_index++){
+				for (int s_index = 0; s_index < Np; s_index++){	/* This 0 and 1 below is for Q-I */
+					temp[0] += DFT_signal[s_index][rx][f_index][0] * wh_sequence[tx][s_index][f_index][0] + DFT_signal[s_index][rx][f_index][1] * wh_sequence[tx][s_index][f_index][1];
+					temp[1] += DFT_signal[s_index][rx][f_index][1] * wh_sequence[tx][s_index][f_index][0] - DFT_signal[s_index][rx][f_index][0] * wh_sequence[tx][s_index][f_index][1];
+					whsum += sqr(wh_sequence[tx][s_index][f_index][0]) + sqr(wh_sequence[tx][s_index][f_index][1]);
+				}
+				H[f_index][rx][tx][0] = temp[0] / whsum;
+				H[f_index][rx][tx][1] = temp[1] / whsum;
+				temp[0] = 0; temp[1] = 0; whsum = 0;
+			}
+		}
+	}
+}
+
+/* For integer power calculation: */
+int intPow(int x, int p)
+{
+	if (p == 0) return 1;
+	if (p == 1) return x;
+
+	int tmp = intPow(x, p / 2);
+	if (p % 2 == 0) return tmp * tmp;
+	else return x * tmp * tmp;
+}
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include "const.h"
+double CNR, EbNo;
+int loop;
+int maxloop;
+/* TS : time slot index. BS is for BS when TS = 0, and it is for relay when TS = 1. */
+double wh_sequence[BS][Np][N][2];
+double transmitted_sigmat[TS][BS][BURST][2];	/* This combines all transmitted singal into matrix form. */
+double mse_sum[LOOPN][NUMBER_OF_STEPS];	/* To print out average MSE */
+extern errno_t err;	/* Error vector */
+
+void main(){
+	double transmitted_signal[BS][BURST][2];	/* Signal of BS1 */
+	double relay_tx_signal[BS][BURST][2];	/* Signal relayed from RL1 */
+	double transmitted_sigvec[TS][BURST][2];	/* Received signal at MS1 after multipath propogation. */
+	int transmitted_bit[BS][Nd][N_dbps];
+	int received_bit[Nd][N_dbps];
+	int bs, rs_bit[BS][Nd][N_dbps];
+
+
+	/*~~~~~~~~~Initialize~begin~~~~~~~~~~~~~~~~~~~~~~~~*/
+	setting_print();	/* Lots of printf */
+	srand((unsigned)time(NULL));
+	FFT_initialize();	/* Construct an FFT matrix */
+	state_model_convolutional();	/* Function and parameters of convolutional encoder */
+
+	transmitter_initialize(transmitted_sigmat);	/* Set transmitted signal vector to zero */
+#ifdef WH_PREAMBLE	/* for 2 user */
+	WHadamard_initialize(transmitted_sigmat, wh_sequence);	/* For walsh hadamart sequence */
+#else
+	Zadoff_Chu_layer_initialize();	/* Most likely: generate two ZC sequences. */
+	Zadoff_Chu_generator_layer1(transmitted_sigmat); /* These two is for TS. */
+	Zadoff_Chu_generator_layer2(transmitted_sigmat);
+#endif // WH
+	/*~~~~~~~~initialize end~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	for (EbNo = EbNo_FROM; EbNo >= EbNo_TO; EbNo -= 5.0){	/* begin :: EbNo */
+		CNR = (double)EbNo + 3.0 * MODULATION / 2.0;
+#ifdef ADPTV_LOOP
+		maxloop = intPow((int)EbNo, 2) + 10;
+#else
+		maxloop = LOOPN;
+#endif // ADPTV_LOOP
+		for (loop = 0; loop < maxloop; loop++){	/* begin :: loop */
+			for (size_t rs = 0; rs < BS; rs++)
+			{
+				bs = rs;
+				transmitter(transmitted_bit[bs], transmitted_signal[bs], bs);
+				relay(transmitted_signal[rs], relay_tx_signal[rs], rs_bit[rs], rs);	/* I see no difference */
+			}
+			transmitted_signal_Matrix(transmitted_signal, relay_tx_signal, transmitted_sigmat);	/* Combine those for into the fifth */
+			multipath_propagation(transmitted_sigmat, transmitted_sigvec);	/* Signal propogates thru channel. We also need the signal which gone thru an ideal channel*/
+#ifdef NOISE
+			for (size_t ts = 0; ts < TS; ts++)
+			{
+#ifdef PROPAGATION
+				noise_addition(0, transmitted_sigvec[ts], transmitted_sigvec[ts]);	/* Add noise to first? time slot. CNR=0 bcs it is included in propogation */
+#else
+				noise_addition((ts == 1) ? CNR : (CNR - LOSS), transmitted_sigvec[ts], transmitted_sigvec[ts]);	/* Add noise to first? time slot. CNR=0 bcs it is included in propogation */
+#endif
+			}
+#endif // NOISE
+			receiver(transmitted_sigvec, received_bit);
+			BER_RsPlusBs(loop, rs_bit[0], transmitted_bit[0], received_bit);
+		}	/* end :: loop */
+	}	/* end :: EbNo */
+}	/* end :: main() */
