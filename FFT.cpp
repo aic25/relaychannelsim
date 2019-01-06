ab6f980aaccc13641de20c034bea3d00abea8c69
canbolat <ihsan25@gmail.com>
2019-01-06 20:56:43 +0900

    initial commit

---
+++ FFT.cpp
@@ -0,0 +1,325 @@
+#include "const.h"
+double Wf[Nf / 2][2];
+double Wf_N[N / 2][2];
+
+void FFT_initialize(){
+	int i;
+	double phase;
+
+	Wf[0][0] = 1.0;
+	Wf[0][1] = 0.0;
+	for (i = 1; i < Nf / 2; i++){
+		phase = (double)i*2.0 / Nf*PI;
+		Wf[i][0] = cos(phase);
+		Wf[i][1] = sin(phase);
+	}
+
+	Wf_N[0][0] = 1.0;
+	Wf_N[0][1] = 0.0;
+	for (i = 1; i < N / 2; i++){
+		phase = (double)i*2.0 / N*PI;
+		Wf_N[i][0] = cos(phase);
+		Wf_N[i][1] = sin(phase);
+	}
+}
+
+void IFFT(double(*frequency_signal)[2], double(*time_signal)[2]){
+	int i, j, j1, j2, it, iter, k, kk, xp, xp2, xp3, carrier, carrier1, carrier2, m;
+	double temp[2], temp1[2], temp2[2];
+
+	for (iter = 0, i = Nf;; iter++){
+		if ((i >>= 1) == 0) break;
+	}
+
+	xp2 = Nf;
+	xp3 = 1;
+	for (it = 0; it < iter; it++){
+		xp = xp2;
+		xp2 = (xp2 >> 1);
+		if (it != 0){
+			xp3 = (xp3 << 1);
+		}
+		for (k = 0; k < xp2; k++){
+			if (k != 0){
+				carrier = k*xp3;
+			}
+			i = k - xp;
+			for (j = xp; j <= Nf; j += xp){
+				carrier1 = j + i;
+				carrier2 = carrier1 + xp2;
+				for (kk = 0; kk < 2; kk++){
+					temp1[kk] = frequency_signal[carrier1][kk];
+					temp2[kk] = frequency_signal[carrier2][kk];
+
+					temp[kk] = temp1[kk] - temp2[kk];
+				}
+				frequency_signal[carrier1][0] = temp1[0] + temp2[0];
+				frequency_signal[carrier1][1] = temp1[1] + temp2[1];
+
+				if (k == 0){
+					frequency_signal[carrier2][0] = temp[0];
+					frequency_signal[carrier2][1] = temp[1];
+				}
+				else {
+					frequency_signal[carrier2][0] = temp[0] * Wf[carrier][0] - temp[1] * Wf[carrier][1];
+					frequency_signal[carrier2][1] = temp[0] * Wf[carrier][1] + temp[1] * Wf[carrier][0];
+				}
+			}
+		}
+	}
+	/********Bit Reversal to {0,1, ...... ,Nf} **********/
+	j1 = Nf / 2;
+	j2 = Nf - 1;
+	j = 1;
+
+	for (i = 1; i < j2; i++){
+		if (i < j){
+			carrier1 = i - 1;
+			carrier2 = j - 1;
+			for (kk = 0; kk < 2; kk++){
+				temp[kk] = frequency_signal[carrier1][kk];
+				frequency_signal[carrier1][kk] = frequency_signal[carrier2][kk];
+				frequency_signal[carrier2][kk] = temp[kk];
+			}
+		}
+		k = j1;
+
+		while (k < j){
+			j -= k;
+			k >>= 1;
+		}
+
+		j += k;
+	}
+
+	for (m = 0; m < Nf; m++){
+		time_signal[m][0] = sqrt((double)1.0 / Nf) * frequency_signal[m][0];
+		time_signal[m][1] = sqrt((double)1.0 / Nf) * frequency_signal[m][1];
+	}
+}
+
+
+void FFT(double(*time_signal)[2], double(*frequency_signal)[2]){
+	int i, j, j1, j2, it, iter, k, kk, xp, xp2, xp3, carrier, carrier1, carrier2, carrier_index;
+	double temp[2], temp1[2], temp2[2];
+
+	for (iter = 0, i = Nf;; iter++){
+		if ((i >>= 1) == 0) break;
+	}
+
+	xp2 = Nf;
+	xp3 = 1;
+	for (it = 0; it < iter; it++){
+		xp = xp2;
+		xp2 = (xp2 >> 1);
+		if (it != 0){
+			xp3 = (xp3 << 1);
+		}
+		for (k = 0; k < xp2; k++){
+			if (k != 0){
+				carrier = k*xp3;
+			}
+			i = k - xp;
+			for (j = xp; j <= Nf; j += xp){
+				carrier1 = j + i;
+				carrier2 = carrier1 + xp2;
+				for (kk = 0; kk < 2; kk++){
+					temp1[kk] = time_signal[carrier1][kk];
+					temp2[kk] = time_signal[carrier2][kk];
+
+					temp[kk] = temp1[kk] - temp2[kk];
+				}
+				time_signal[carrier1][0] = temp1[0] + temp2[0];
+				time_signal[carrier1][1] = temp1[1] + temp2[1];
+
+				if (k == 0){
+					time_signal[carrier2][0] = temp[0];
+					time_signal[carrier2][1] = temp[1];
+				}
+				else {
+					time_signal[carrier2][0] = temp[0] * Wf[carrier][0] + temp[1] * Wf[carrier][1];
+					time_signal[carrier2][1] = -temp[0] * Wf[carrier][1] + temp[1] * Wf[carrier][0];
+				}
+			}
+		}
+	}
+	/********Bit Reversal to {0,1, ...... ,Nf} **********/
+	j1 = Nf / 2;
+	j2 = Nf - 1;
+	j = 1;
+
+	for (i = 1; i < j2; i++){
+		if (i < j){
+			carrier1 = i - 1;
+			carrier2 = j - 1;
+			for (kk = 0; kk < 2; kk++){
+				temp[kk] = time_signal[carrier1][kk];
+				time_signal[carrier1][kk] = time_signal[carrier2][kk];
+				time_signal[carrier2][kk] = temp[kk];
+			}
+		}
+		k = j1;
+
+		while (k < j){
+			j -= k;
+			k >>= 1;
+		}
+
+		j += k;
+	}
+
+	for (carrier_index = 0; carrier_index < Nf; carrier_index++){
+		for (kk = 0; kk < 2; kk++){
+			frequency_signal[carrier_index][kk] = sqrt((double)1.0 / Nf) * time_signal[carrier_index][kk];
+		}
+	}
+}
+
+void IFFT_N(double(*frequency_signal)[2], double(*time_signal)[2]){
+	int i, j, j1, j2, it, iter, k, kk, xp, xp2, xp3, carrier, carrier1, carrier2, m;
+	double temp[2], temp1[2], temp2[2];
+
+	for (iter = 0, i = N;; iter++){
+		if ((i >>= 1) == 0) break;
+	}
+
+	xp2 = N;
+	xp3 = 1;
+	for (it = 0; it < iter; it++){
+		xp = xp2;
+		xp2 = (xp2 >> 1);
+		if (it != 0){
+			xp3 = (xp3 << 1);
+		}
+		for (k = 0; k < xp2; k++){
+			if (k != 0){
+				carrier = k*xp3;
+			}
+			i = k - xp;
+			for (j = xp; j <= N; j += xp){
+				carrier1 = j + i;
+				carrier2 = carrier1 + xp2;
+				for (kk = 0; kk < 2; kk++){
+					temp1[kk] = frequency_signal[carrier1][kk];
+					temp2[kk] = frequency_signal[carrier2][kk];
+
+					temp[kk] = temp1[kk] - temp2[kk];
+				}
+				frequency_signal[carrier1][0] = temp1[0] + temp2[0];
+				frequency_signal[carrier1][1] = temp1[1] + temp2[1];
+
+				if (k == 0){
+					frequency_signal[carrier2][0] = temp[0];
+					frequency_signal[carrier2][1] = temp[1];
+				}
+				else {
+					frequency_signal[carrier2][0] = temp[0] * Wf_N[carrier][0] - temp[1] * Wf_N[carrier][1];
+					frequency_signal[carrier2][1] = temp[0] * Wf_N[carrier][1] + temp[1] * Wf_N[carrier][0];
+				}
+			}
+		}
+	}
+	/********Bit Reversal to {0,1, ...... ,N} **********/
+	j1 = N / 2;
+	j2 = N - 1;
+	j = 1;
+
+	for (i = 1; i < j2; i++){
+		if (i < j){
+			carrier1 = i - 1;
+			carrier2 = j - 1;
+			for (kk = 0; kk < 2; kk++){
+				temp[kk] = frequency_signal[carrier1][kk];
+				frequency_signal[carrier1][kk] = frequency_signal[carrier2][kk];
+				frequency_signal[carrier2][kk] = temp[kk];
+			}
+		}
+		k = j1;
+
+		while (k < j){
+			j -= k;
+			k >>= 1;
+		}
+
+		j += k;
+	}
+
+	for (m = 0; m < N; m++){
+		time_signal[m][0] = sqrt((double)1.0 / N) * frequency_signal[m][0];
+		time_signal[m][1] = sqrt((double)1.0 / N) * frequency_signal[m][1];
+	}
+}
+
+void FFT_N(double(*time_signal)[2], double(*frequency_signal)[2]){ //for signal at zc channel estimation
+	int i, j, j1, j2, it, iter, k, kk, xp, xp2, xp3, carrier, carrier1, carrier2, carrier_index;
+	double temp[2], temp1[2], temp2[2];
+
+	for (iter = 0, i = N;; iter++){
+		if ((i >>= 1) == 0) break;
+	}
+
+	xp2 = N;
+	xp3 = 1;
+	for (it = 0; it < iter; it++){
+		xp = xp2;
+		xp2 = (xp2 >> 1);
+		if (it != 0){
+			xp3 = (xp3 << 1);
+		}
+		for (k = 0; k < xp2; k++){
+			if (k != 0){
+				carrier = k*xp3;
+			}
+			i = k - xp;
+			for (j = xp; j <= N; j += xp){
+				carrier1 = j + i;
+				carrier2 = carrier1 + xp2;
+				for (kk = 0; kk < 2; kk++){
+					temp1[kk] = time_signal[carrier1][kk];
+					temp2[kk] = time_signal[carrier2][kk];
+
+					temp[kk] = temp1[kk] - temp2[kk];
+				}
+				time_signal[carrier1][0] = temp1[0] + temp2[0];
+				time_signal[carrier1][1] = temp1[1] + temp2[1];
+
+				if (k == 0){
+					time_signal[carrier2][0] = temp[0];
+					time_signal[carrier2][1] = temp[1];
+				}
+				else {
+					time_signal[carrier2][0] = temp[0] * Wf_N[carrier][0] + temp[1] * Wf_N[carrier][1];
+					time_signal[carrier2][1] = -temp[0] * Wf_N[carrier][1] + temp[1] * Wf_N[carrier][0];
+				}
+			}
+		}
+	}
+	/********Bit Reversal to {0,1, ...... ,N} **********/
+	j1 = N / 2;
+	j2 = N - 1;
+	j = 1;
+
+	for (i = 1; i < j2; i++){
+		if (i < j){
+			carrier1 = i - 1;
+			carrier2 = j - 1;
+			for (kk = 0; kk < 2; kk++){
+				temp[kk] = time_signal[carrier1][kk];
+				time_signal[carrier1][kk] = time_signal[carrier2][kk];
+				time_signal[carrier2][kk] = temp[kk];
+			}
+		}
+		k = j1;
+		while (k < j){
+			j -= k;
+			k >>= 1;
+		}
+		j += k;
+	}
+
+	for (carrier_index = 0; carrier_index < N; carrier_index++){
+		for (kk = 0; kk < 2; kk++){
+			frequency_signal[carrier_index][kk] = sqrt((double)1.0 / N) * time_signal[carrier_index][kk];
+		}
+	}
+}
---
+++ MRC.cpp
@@ -0,0 +1,84 @@
+#include "const.h"
+double her[N][TS][2], channel_power[N][2];
+extern double CNR;
+extern double channel_gain[N][2], p_in[N];
+
+void mrc_weight(double(*H)[TS][BS][2]){	/* Here, w is selected as h; a kind of matched filter. */
+	double h[N][TS][2], Hher[BS][TS][2], R[BS][BS][2], power[2], poweri[2], no_loss, no;
+
+	h_desired(H, h);	/* Choose the channel of desired user */
+	VectorHermite(h, her);	/* her = h^H */
+	VectorMulVectorToscalar(her, h, channel_power);	/* channel_power = h^H x h */
+#ifdef SD
+	no = pow(10.0, -CNR / 10.0) / CODING_RATE;
+	no_loss = pow(10.0, -(CNR - LOSS) / 10.0) / CODING_RATE;
+	for (size_t f_index = 0; f_index < N; f_index++)
+	{
+		power[0] = her[f_index][0][0] * her[f_index][0][0] + her[f_index][0][1] * her[f_index][0][1];
+		power[1] = her[f_index][1][0] * her[f_index][1][0] + her[f_index][1][1] * her[f_index][1][1];
+		MatrixHermite_TSxBS(H[f_index], Hher);
+		MatrixMulMatrixToMatrix_BSxTS(Hher, H[f_index], R);
+		poweri[0] = 0;
+		for (size_t i = 1; i < M; i++)
+		{
+			poweri[0] += sqr(R[0][i][0]) + sqr(R[0][i][1]);	/* 0.5=no */
+		}
+		channel_gain[f_index][0] = R[0][0][0];
+		channel_gain[f_index][1] = -R[0][0][1];
+		p_in[f_index] = (no_loss)*power[0] + (no)*power[1] + poweri[0];
+	}
+#endif // SD
+
+}
+
+
+void h_desired(double(*H)[TS][BS][2], double(*h_m)[TS][2]){	
+	int ts, i;
+	for (ts = 0; ts < TS; ts++)
+	{
+		for (i = 0; i < N; i++)
+		{
+			h_m[i][ts][0] = H[i][ts][0][0];
+			h_m[i][ts][1] = H[i][ts][0][1];
+		}
+	}
+}
+
+void VectorHermite(double(*h)[TS][2], double(*her)[TS][2]){
+	int i, ts;
+
+	for (i = 0; i < N; i++)
+	{
+		for (ts = 0; ts < TS; ts++)
+		{
+			her[i][ts][0] = h[i][ts][0];
+			her[i][ts][1] = -h[i][ts][1];
+		}
+	}
+}
+
+
+void VectorMulVectorToscalar(double(*her)[TS][2], double(*h)[TS][2], double(*channel_power)[2]){
+	int ts, n;
+	double sum[2];
+	for (n = 0; n < N; n++)
+	{
+		sum[0] = 0;
+		sum[1] = 0;
+		for (ts = 0; ts < TS; ts++)
+		{
+			sum[0] += her[n][ts][0] * h[n][ts][0] - her[n][ts][1] * h[n][ts][1];
+			sum[1] += her[n][ts][0] * h[n][ts][1] + her[n][ts][1] * h[n][ts][0];
+		}
+		channel_power[n][0] = sum[0];
+		channel_power[n][1] = sum[1];
+	}
+}
+
+
+
+
+
+
+
+
